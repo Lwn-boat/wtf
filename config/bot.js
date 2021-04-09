7c9232febf393d16cbc390656b4488737f2ef488
RayC'e#1234 <77170807+rayce1234@users.noreply.github.com>
2021-04-09 23:54:22 +0900

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODMwMDkxMjk1NTYwNDMzNzQ0.YHBoww.ZfE3YyaVi_SfBIPjn8MyeYPw52w',
+        prefix: 't.',
+        activity: 't.help',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
